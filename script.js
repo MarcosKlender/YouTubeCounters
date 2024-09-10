8074abb96aeb49600a2fbb4cc1348b41e2cd9101
Klender Carrasco <marcosklender@gmail.com>
2024-09-10 09:35:43 -0500

    Refactor code and add favicon

--- script.js
+++ script.js
@@ -4,86 +4,95 @@ document.addEventListener("DOMContentLoaded", () => {
   const channelForm = document.getElementById("channelForm");
   const channelSearch = document.getElementById("channelSearch");
   const channelInfo = document.getElementById("channelInfo");
+  const errorMessage = document.getElementById("errorMessage");
 
-  searchButton.addEventListener("click", searchChannel);
-  channelSearch.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      searchChannel();
-    }
-  });
-
-  function searchChannel() {
+  const handleSearch = () => {
     const channelName = channelSearch.value.trim();
     if (!channelName) {
-      // Mostrar un mensaje de error si el campo está vacío
-      channelInfo.innerHTML =
-        '<p class="text-center text-red-500 font-bold">*Por favor, ingresa el nombre del canal.</p>';
+      displayError("*Por favor, ingresa el nombre del canal.");
       return;
     }
 
-    // Limpiar el mensaje de error anterior si había uno
-    channelInfo.innerHTML = "";
+    // Limpia el mensaje de error si ya se ha mostrado
+    clearError();
 
-    // Realiza una solicitud para buscar el canal por nombre
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${channelName}&type=channel`
-    )
-      .then((response) =>
-        response.ok
-          ? response.json()
-          : Promise.reject("Error en la solicitud de búsqueda de canal.")
-      )
+    // Continuar con la búsqueda
+    searchChannel(channelName);
+  };
+
+  // Agrega listeners para buscar el canal
+  searchButton.addEventListener("click", handleSearch);
+  channelSearch.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") handleSearch();
+  });
+
+  const displayError = (message) => {
+    errorMessage.textContent = message;
+  };
+
+  const clearError = () => {
+    errorMessage.textContent = "";
+  };
+
+  const searchChannel = (channelName) => {
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${channelName}&type=channel`;
+    fetch(url)
+      .then(handleResponse)
       .then((data) => {
         if (data.items.length === 0) {
-          channelInfo.innerHTML =
-            '<p class="text-center text-red-500 font-bold">No se encontró ningún canal con ese nombre.</p>';
-          return;
+          displayError("No se encontró ningún canal con ese nombre.");
         }
 
         const channelId = data.items[0].id.channelId;
-        getChannelInfo(channelId);
+        fetchChannelInfo(channelId);
       })
-      .catch((error) => console.error("Error:", error));
-  }
+      .catch(handleError);
+  };
 
-  const getChannelInfo = (channelId) => {
-    fetch(
-      `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${API_KEY}`
-    )
-      .then((response) =>
-        response.ok
-          ? response.json()
-          : Promise.reject("Error en la solicitud a la API de YouTube.")
-      )
+  const fetchChannelInfo = (channelId) => {
+    const url = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${API_KEY}`;
+    fetch(url)
+      .then(handleResponse)
       .then((data) => {
         const { snippet, statistics } = data.items[0];
-        const profilePictureUrl = snippet.thumbnails.medium.url;
-        const subscriberCount = parseInt(
-          statistics.subscriberCount
-        ).toLocaleString();
-        const viewCount = parseInt(statistics.viewCount).toLocaleString();
-        const channelName = snippet.title;
-
-        channelForm.remove();
-
-        channelInfo.innerHTML = `
-        <div class="bg-white p-6 rounded-lg shadow-lg text-center">
-            <h2 class="text-2xl font-bold">${channelName}</h2>
-            <div class="flex items-center justify-center">
-                <img class="my-4 rounded-lg" src="${profilePictureUrl}" alt="Perfil de ${channelName}">
-            </div>
-
-            <p class="text-gray-600">Suscriptores</p>
-            <p class="text-xl font-semibold">${subscriberCount}</p>
-            <p class="text-gray-600">Total de Visitas</p>
-            <p class="text-xl font-semibold">${viewCount}</p>
-
-            <button onClick="window.location.reload();"
-            class="bg-red-700 text-white mt-4 px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:bg-red-600">
-            Reiniciar</button>
-        </div>
-                `;
+        renderChannelInfo(snippet, statistics);
       })
-      .catch((error) => console.error("Error:", error));
+      .catch(handleError);
+  };
+
+  const handleResponse = (response) => {
+    if (!response.ok) {
+      throw new Error("Error en la solicitud a la API.");
+    }
+    return response.json();
+  };
+
+  const handleError = (error) => {
+    console.error("Error:", error);
+  };
+
+  const renderChannelInfo = (snippet, statistics) => {
+    const { thumbnails, title } = snippet;
+    const { subscriberCount, viewCount } = statistics;
+    const formattedSubscribers = parseInt(subscriberCount).toLocaleString();
+    const formattedViews = parseInt(viewCount).toLocaleString();
+
+    channelForm.remove();
+    channelInfo.innerHTML = `
+      <div class="bg-white p-6 rounded-lg shadow-lg text-center">
+        <h2 class="text-2xl font-bold">${title}</h2>
+        <div class="flex items-center justify-center">
+          <img class="my-4 rounded-lg" src="${thumbnails.medium.url}" alt="Perfil de ${title}">
+        </div>
+        <p class="text-gray-600">Suscriptores</p>
+        <p class="text-xl font-semibold">${formattedSubscribers}</p>
+        <p class="text-gray-600">Total de Visitas</p>
+        <p class="text-xl font-semibold">${formattedViews}</p>
+        <button onClick="window.location.reload();"
+          class="bg-red-700 text-white mt-4 px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:bg-red-600">
+          Reiniciar
+        </button>
+      </div>
+    `;
   };
 });
